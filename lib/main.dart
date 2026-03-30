cd6a41e0d256983df6430cd3d62b120d501abf4b
emilejwayservice <emile@jway.ma>
2026-03-30 12:39:46 +0100

    fix : push this modification

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pda/core/dependencies/dependencies.dart';
 import 'package:pda/core/services/shared_pref_service.dart';
-import 'package:pda/data/data_providers/local_db/local_db.dart';
-import 'package:pda/domain/entities/product.dart';
-import 'package:pda/domain/repository/repository.dart';
 import 'package:pda/presentation/blocs/core_bloc/core_bloc.dart';
+
 import 'core/themes/light_theme/light_theme.dart';
 import 'core/utils/set_repository.dart';
 import 'routes.dart';
 
-
-
-void main() async{
- WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await prepareDependencies();
   runApp(const MyApp());
-
 }
 
-
-
-Future<void> prepareDependencies()async{
-  SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarBrightness: Brightness.dark,
-          statusBarIconBrightness: Brightness.dark
-      )
-  );
+Future<void> prepareDependencies() async {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark));
   SystemChrome.setPreferredOrientations(
-    [
-      DeviceOrientation.portraitDown,
-      DeviceOrientation.portraitUp
-    ]
-  );
-  SharedPrefService sharedPrefService=await SharedPrefService.initializeService();
-  CoreBloc coreBloc=CoreBloc();
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  SharedPrefService sharedPrefService =
+      await SharedPrefService.initializeService();
+  CoreBloc coreBloc = CoreBloc();
   Dependencies.put(coreBloc);
   Dependencies.put(sharedPrefService);
   await setRepository();
@@ -45,20 +32,14 @@ Future<void> prepareDependencies()async{
   /*Repository repository=Dependencies.get<Repository>();
   List<ProductEntity> products=await repository.getProductsNotSent();
   products.forEach((element) {print(element.toString());});*/
-
 }
-
-
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return  MediaQuery(
+    return MediaQuery(
       data: MediaQuery.of(context).copyWith(textScaler: TextScaler.noScaling),
       child: MaterialApp.router(
         title: 'PVM',
@@ -69,5 +50,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
